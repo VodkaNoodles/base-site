f5ea291a6d9a053f3487f1d911208a647fc1cadf
jacobhasason <154370419+jacobhasason@users.noreply.github.com>
2026-04-14 19:21:36 -0700

    4/14 - Test Push
    Made minor changes to the original test branch to test if I can make changes easily.

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "stylesheet" href ="styles.css">
     <title>AI and Visual Knowledge Discovery</title>
     <link rel="stylesheet" href="styles.css">
 </head>
@@ -14,4 +15,7 @@
         <h2>Sources</h2>
         <ul>
             <li><a href="http://www.cwu.edu/~borisk">CWU website of Prof. Boris Kovalerchuk</a></li>
-            <li><a
+        </ul>
+    </main>
+</body>
+                    
